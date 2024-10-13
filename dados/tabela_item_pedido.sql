820389f76f3a9bb7bc8ade1ab0a4659b9fe5cc5b
alinelima2799 <alinelima.2799@gmail.com>
2024-10-13 17:53:19 -0300

    ajuste

--- dados/tabela_item_pedido.sql
+++ dados/tabela_item_pedido.sql
@@ -1,8 +1,10 @@
 CREATE TABLE cafe_mania_db.item_pedido (
     id_item_pedido INT NOT NULL AUTO_INCREMENT,
-    id_pedido INT REFERENCES pedidos(id_pedido),
-    id_produto INT REFERENCES produtos(id_produto),
+    id_pedido INT DEFAULT NULL,
+    id_produto INT DEFAULT NULL,
     quantidade_item INT,
     preco_total_item DECIMAL(10, 2),
-    PRIMARY KEY (id_item_pedido)
+    PRIMARY KEY (id_item_pedido),
+    FOREIGN KEY (id_pedido) REFERENCES cafe_mania_db.pedidos_clientes(id_cliente),
+    FOREIGN KEY (id_produto) REFERENCES cafe_mania_db.produtos_estoque(id_produto)
 );
