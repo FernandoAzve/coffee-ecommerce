8ba518acd65ab9cf15cf0744e55625f2736db456
alinelima2799 <alinelima.2799@gmail.com>
2024-10-13 16:35:06 -0300

    Ajuste nome da tabela e criação da tabela carrinho

--- dados/tabela_pedidos_clientes.sql
+++ dados/tabela_pedidos_clientes.sql
@@ -1,4 +1,4 @@
-CREATE TABLE cafe_mania_db.pedidos (
+CREATE TABLE cafe_mania_db.pedidos_clientes (
     id_pedido INT NOT NULL AUTO_INCREMENT,
     id_status INT,
     id_cliente INT,
@@ -7,6 +7,6 @@ CREATE TABLE cafe_mania_db.pedidos (
     data_pedido TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id_pedido),
     FOREIGN KEY (id_status) REFERENCES cafe_mania_db.status_pedido(id_status),
-    FOREIGN KEY (id_cliente) REFERENCES cafe_mania_db.clientes(id_cliente),
-    FOREIGN KEY (id_produto) REFERENCES cafe_mania_db.produtos(id_produto)
+    FOREIGN KEY (id_cliente) REFERENCES cafe_mania_db.cadastro_clientes(id_cliente),
+    FOREIGN KEY (id_produto) REFERENCES cafe_mania_db.produtos_estoque(id_produto)
 );
