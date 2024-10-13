d206c35b572541ddbb418e81340fb5b3f5e22f53
alinelima2799 <alinelima.2799@gmail.com>
2024-10-13 18:34:43 -0300

    Modelagem de dados e Ajusta tabela pedidos

--- dados/tabela_pedidos_clientes.sql
+++ dados/tabela_pedidos_clientes.sql
@@ -2,11 +2,9 @@ CREATE TABLE cafe_mania_db.pedidos_clientes (
     id_pedido INT NOT NULL AUTO_INCREMENT,
     id_status INT,
     id_cliente INT,
-    id_produto INT,
     valor_compra DECIMAL(14,2) NOT NULL,
     data_pedido TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id_pedido),
     FOREIGN KEY (id_status) REFERENCES cafe_mania_db.status_pedido(id_status),
-    FOREIGN KEY (id_cliente) REFERENCES cafe_mania_db.cadastro_clientes(id_cliente),
-    FOREIGN KEY (id_produto) REFERENCES cafe_mania_db.produtos_estoque(id_produto)
+    FOREIGN KEY (id_cliente) REFERENCES cafe_mania_db.cadastro_clientes(id_cliente)
 );
